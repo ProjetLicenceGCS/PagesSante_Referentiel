27af456c6e38f4db7cbd04e0ce9c3f056238f485
damienChesneau <ches.damien@gmail.com>
2013-12-16 11:31:58 +0100

    ajout exeption SystemNotSupportedException

---
+++ src/main/java/com/emosist/pagessante/exception/SystemNotSupportedException.java
@@ -0,0 +1,14 @@
+
+package com.emosist.pagessante.exception;
+
+/**
+ *
+ * @author Damien Chesneau <contact@damienchesneau.fr>
+ */
+public class SystemNotSupportedException extends PageSanteException{
+
+    public SystemNotSupportedException(String message) {
+        super(message);
+    }
+
+}
